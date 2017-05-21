ae53c60abce422b88acafe277a772c055f056962
mwcweb1 <mwcweb1@outlook.com>
2017-05-21 13:33:30 +0200

    Revert "ffj"
    This reverts commit aa1750de1ace2b20c235a70c0b723e2ec9985e9d.

--- tesla/Layout.Master.designer.cs
+++ tesla/Layout.Master.designer.cs
@@ -31,6 +31,15 @@ namespace tesla {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+        
+        /// <summary>
         /// Repeater_menu control.
         /// </summary>
         /// <remarks>
